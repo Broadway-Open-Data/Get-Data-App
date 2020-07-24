f45e9b0fc73c939123f060964b048f04e923273c
ybressler <yaakovbressler@gmail.com>
2020-07-24 00:16:25 -0400

    added the migrations file

--- app/databases/db.py
+++ app/databases/db.py
@@ -15,7 +15,7 @@ db = SQLAlchemy()
 class User(db.Model):
     """"""
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True, nullable=False, default=lambda: int(str(int(uuid.uuid4()))[:7]), unique=True, index=True)
+    id = db.Column(db.Integer, primary_key=True,unique=True)
     date_instantiated = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(200), nullable=False)
