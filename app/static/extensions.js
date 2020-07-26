fff9693342551b744c4a7542c32e98345d06782c
ybressler <yaakovbressler@gmail.com>
2020-07-26 12:45:03 -0400

    reformatted styles

---
+++ app/static/extensions.js
@@ -0,0 +1,7 @@
+// Enable js to hide content on the page...
+
+function closeDiv(){
+   //do something
+   var x = document.getElementById("close-alert");
+   x.parentNode.style.display = 'none';
+}
