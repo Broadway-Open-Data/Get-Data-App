2823b8b1390fb3a817655d2b3c111e9bd3694ee2
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-09-12 15:03:27 -0400

    added switch case to change handler

--- app/static/js/user-interactions.js
+++ app/static/js/user-interactions.js
@@ -2,16 +2,24 @@
 // Activate the toggle switch
 $(document).ready(function() {
    $('.toggle').change(function(e) {
-    //  console.log("TTTTTTTTTAAAAAAAAAARRRRRRRRRRRRGGGGGGGGGGGGEEEEEEEEEETTTTTTTT", e.target.value)
-      var current_status = $('.status').text();
+     let current_status
+    //  var current_status = $('.status').text();
 
+     switch (e.target.id) {
+      case 'develop':
+        current_status = 'Developer Mode';
+          break;
+      case 'analyst':
+        current_status = 'Analyst Mode';
+          break;
+        }
       // Update the toggle value
       $.ajax({
        url: "/get_toggled_status",
        type: "get",
         data: {status: current_status},
         success: function(response) {
-         $(".status").html(response);
+        //  $(".status").html(response);
          $(".dev-mode").html(response);
         },
         error: function(xhr) {
