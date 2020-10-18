0267c8e3e6b1a734ecdd574520013cf59df97a0e
ybressler <yaakovbressler@gmail.com>
2020-10-18 14:18:55 -0400

    added bind keys to tables.

--- app/databases/models/users/__init__.py
+++ app/databases/models/users/__init__.py
@@ -1,3 +1,6 @@
+from databases.models import dbTable
+user_dbTable = dbTable
+
 from .form_messages import *
 from .roles import *
 from .users import *
--- app/databases/models/users/form_messages.py
+++ app/databases/models/users/form_messages.py
@@ -9,9 +9,17 @@ import datetime
 # Define models
 
 
+# class messages_users(db.Table):
+#     __tablename__='messages_users'
+#     __bind_key__ = "users"
+#     user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+#     message_id = db.Column(db.Integer(), db.ForeignKey('message.id'))
+
 messages_users = db.Table('messages_users',
         db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-        db.Column('message_id', db.Integer(), db.ForeignKey('message.id')))
+        db.Column('message_id', db.Integer(), db.ForeignKey('message.id')),
+        info={'bind_key': 'users'}
+        )
 
 
 # ------------------------------------------------------------------------------
