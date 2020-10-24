0cb2d4cb2fad9a8e588eeeb65d88580c335e4e54
ybressler <yaakovbressler@gmail.com>
2020-10-23 22:04:12 -0400

    fixing one tiny change with nginx

--- nginx/nginx-start.sh
+++ nginx/nginx-start.sh
@@ -1,3 +1,4 @@
+# On ec2, run this file in a detached session (screen -S nginx)
 # Run this command from this file...
 echo "**** Server is running! ****"
 sudo nginx -c $(pwd)/nginx.conf -g 'daemon off;' -p $(pwd)
@@ -7,7 +8,3 @@ sudo nginx -c /home/ec2-user/MVP-FrontEnd/nginx/nginx.conf -g 'daemon off;' -p  
 
 # Am currently using the following to reload my nginx
 sudo service nginx reload
-
-# Might replace with
-# sudo service nginx stop
-# sudo nginx -c /home/ec2-user/MVP-FrontEnd/nginx/nginx.conf -g 'daemon off;' -p  /home/ec2-user/MVP-FrontEnd/
