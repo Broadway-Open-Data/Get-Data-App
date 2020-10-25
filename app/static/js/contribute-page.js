9df6532e29042740dbfcd3cd413b668116f89dec
ybressler <yaakovbressler@gmail.com>
2020-10-24 22:54:46 -0400

    added boilerplate js to the page. #55

---
+++ app/static/js/contribute-page.js
@@ -0,0 +1,30 @@
+
+
+// // Activate data summary
+$(document).ready(function() {
+
+  // Add this in a bit
+  /*
+  // Another function....
+  $('.advanced-label').each(function(e){
+    $(this).on("click", function(){
+
+      // Allow this to work, regardless of id
+      let myId = this.id.replace('label','content')
+      let x = document.getElementById(myId)
+
+      let visible = x.style.display;
+
+      // Toggle visibility
+      if (visible === "none") {
+        x.style.display = "block";
+      }
+      else {
+        x.style.display = "none";
+      }
+    }) // close the on click function
+
+  }) // close the selector function
+*/
+
+})
