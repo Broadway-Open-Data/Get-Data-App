7606026b504f4ff4b90da560453500688d753821
ybressler <yaakovbressler@gmail.com>
2020-10-30 23:07:25 -0400

    comma and space separated vals.

--- app/databases/methods/broadway/people.py
+++ app/databases/methods/broadway/people.py
@@ -200,7 +200,7 @@ def get_all_directors(params, include_show_data_json=False, output_format='html'
         		) AS 'full_name',
         	 DATE_FORMAT(person.date_of_birth, '{dt_format}') AS date_of_birth,
              gender_identity.name as 'gender_identity',
-             GROUP_CONCAT(DISTINCT racial_identity.name) as 'racial_identities',
+             GROUP_CONCAT(DISTINCT racial_identity.name SEPARATOR ', ') as 'racial_identities',
         	 GROUP_CONCAT(JSON_OBJECT('id', shows.id, 'title', shows.title, 'year', shows.year, 'role', role.name)) AS show_data,
         	 COUNT(DISTINCT(shows.id)) AS 'n shows',
              MAX(shows.year)  - MIN(shows.year) AS 'n years directing',
