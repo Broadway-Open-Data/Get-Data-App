f2985b5830dd61b5726f6419b7f0954bdb5dfcb6
ybressler <yaakovbressler@gmail.com>
2020-11-03 12:48:41 -0500

    added a dummy function. #76

--- app/static/js/user-interactions.js
+++ app/static/js/user-interactions.js
@@ -97,7 +97,31 @@ $(document).ready(function() {
 
 
 
+// DATE UPDATE THINGY
+$(document).ready(function() {
 
+  // Another function....
+  $('.advanced-label').each(function(e){
+    $(this).on("click", function(){
+
+      // Allow this to work, regardless of id
+      let myId = this.id.replace('label','content')
+      let x = document.getElementById(myId)
+
+      let visible = x.style.display;
+
+      // Toggle visibility
+      if (visible === "none") {
+        x.style.display = "block";
+      }
+      else {
+        x.style.display = "none";
+      }
+    }) // close the on click function
+
+  }) // close the selector function
+
+})
 
 
 
